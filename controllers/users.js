969c76ce5e8a25bdf6188f18ac23839a78af1d58
DAVIDK-1999 <davidkalina1999@gmail.com>
2020-02-10 18:42:34 -0700

    Begun user routes for express side of things

---
+++ controllers/users.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+
+const bcrypt = require('bcrypt');
+
+// * Register Route 
+router.post('/register', async (req, res) => {
+    const hashedPassword = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+    const userDbEntry = {
+        username: req.body.username, 
+        password: hashedPassword,
+        email: req.body.email
+    }
+    try {
+        console.log('Hi');
+        const createdUser = await User.create(userDbEntry);
+        req.session.username = createdUser.username;
+        req.session.logged = true;
+        res.status(200).json(createdUser);
+        // res.send(data = createdUser, status={code: 200});
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+// * Login Route
+router.post('/login', async (req, res) => {
+    try {
+        const foundUser = await User.findOne({username: req.body.username});
+        if(foundUser) {
+            if(bcrypt.compareSync(req.body.password, foundUser.password)) {
+                req.session.message = '';
+                req.session.username = foundUser.username;
+                req.session.loggedin = true;
+                res.status(200).json(foundUser);
+            } else {
+                req.session.message = 'Username or password is incorrect';
+            }
+        } else {
+            req.session.message = 'Username or password is incorrect';
+        }
+    } catch(err) {
+        res.send(err);
+    }
+});
+
+router.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if(err) {
+            res.send(err);
+        } else {
+            res.status(200).json("Its garbage");
+        }
+    })
+})
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
+const port = 8000;
+const whitelist = ['http://localhost:3000']
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+
+require('./db/db.js');
+
+app.use(cors(corsOptions));
+
+app.use(session({secret: 'asdhlasfhalsjfa',resave: false, saveUninitialized: false}));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+const usersController = require('./controllers/users.js');
+app.use('/auth', usersController);
+
+app.listen(port, () => {console.log(`Listening on port ${port}`)});

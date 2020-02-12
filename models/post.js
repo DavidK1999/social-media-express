f58dc3773015b3dbd1fcfa61321cab4b1a5a14de
DavidK1999 <davidkalina404@gmail.com>
2020-02-12 06:36:49 -0700

    Updated the date time field

--- models/post.js
+++ models/post.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const postSchema = mongoose.Schema({
     user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
     body: {type: String, required: true},
-    timestamp: {type: String, required: true},
+    timestamp: {type: Date, default: Date.now},
     likes: {type: Number, default: 0}
 })
 
